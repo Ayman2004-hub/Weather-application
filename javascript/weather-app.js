83cda8d772ee8e4cbd0bbf835c27de07bd4cbbf5
Ayman Berkane <302126671@student.rocmondriaan.nl>
2024-01-17 04:14:09 -0800

    HTML/CSS/Javascript
    - making up a structure in which the weather data is entered
    - getting references from HTML elements
    - styling and postitioning items and content

--- javascript/weather-app.js
+++ javascript/weather-app.js
@@ -1,1 +1,10 @@
-console.log('loaded')
+// Get references to HTML elements
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.querySelector('.searchBtn');
+const weatherImg = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temperature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity');
+const windSpeed = document.getElementById('wind-speed');
+const locationNotFound = document.querySelector('.location-not-found');
+const weatherBody = document.querySelector('.weather-body');
